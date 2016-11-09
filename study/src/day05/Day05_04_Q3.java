42caeb5aec3a0826c1c48999a0c5e1ca3aa462ec
whwns <whwns@DESKTOP-MLVSGDN>
2016-11-09 22:45:01 +0900

    과제 업대이트

--- study/src/day05/Day05_04_Q3.java
+++ study/src/day05/Day05_04_Q3.java
@@ -1,10 +1,36 @@
 package day05;
 
-public class Day05_04_Q3 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-	public static void main(String[] args) {
+/*
+ * 2011_11_07
+ * 
+ * 사용자로 부터 반지름을 입력받아 원의 넓이를 구하기
+ */
+public class Day05_04_Q3 {
+	
+	final static double pi = Math.PI;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		// TODO Auto-generated method stub
-		System.out.println("Git hub 테스트");
+		
+		 
+		double rad; // 반지름
+		double area; //원의 넒이
+		
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		
+		System.out.print("반지름을 입력하세요 : ");
+		rad = Double.parseDouble(br.readLine());
+		
+		area = rad * rad * pi;
+		System.out.println(rad);
+		System.out.println(area);
+		
 	}
 
 }
